fb02ee46d350e98fd6ee134edaa6b03e6aecca47
luchbheag <nedolivko.julia@gmail.com>
2024-04-10 15:38:09 +0200

    finish IO task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,33 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder sb = new StringBuilder();
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            int ch;
+            while ((ch = fileInputStream.read()) != -1) {
+                sb.append((char)ch);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] pairsOfData = sb.toString().split("\n");
+        String name = getValueFromPair(pairsOfData[0]);
+        int age = Integer.valueOf(getValueFromPair(pairsOfData[1]));
+        String email = getValueFromPair(pairsOfData[2]);
+        long phone = Long.valueOf(getValueFromPair(pairsOfData[3]));
+
+        return new Profile(name, age, email, phone);
+    }
+
+    private String getValueFromPair(String pair) {
+        return pair.split(":")[1].trim();
     }
 }
